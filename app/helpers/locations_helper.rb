b9eb0cc842ddaf2d9330f71a1eb961aa77aa9a75
AI-Mozi <bieniek.milosz0@gmail.com>
2021-03-15 17:25:43 +0100

    Current weather views

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -2,4 +2,12 @@ module LocationsHelper
   def dt(timezone)
     Time.at(timezone).strftime("%d %B %Y")
   end
+
+  def image(code, size)
+    "http://openweathermap.org/img/wn/#{code}#{size}.png"
+  end
+
+  def sun(timezone)
+    Time.at(timezone).strftime("%H:%M")
+  end
 end
